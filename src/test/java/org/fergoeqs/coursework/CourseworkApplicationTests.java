0383e9a2280bf5f58e20649f4e63449ac41d19f4
eoeqs <sashaivanova977@gmail.com>
2025-02-04 15:08:17 +0300

    initial commit

---
+++ src/test/java/org/fergoeqs/coursework/CourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fergoeqs.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
