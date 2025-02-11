9dfb9d9d2bf64c0bdd34c6cbf508d548acb5c79c
fergie <doigralesfergie@gmail.com>
2025-02-12 02:36:59 +0300

    feat(backend): added getting detailed information about the user

---
+++ src/main/java/org/fergoeqs/coursework/dto/AppUserDTO.java
@@ -0,0 +1,18 @@
+package org.fergoeqs.coursework.dto;
+
+import org.fergoeqs.coursework.models.enums.RoleType;
+import java.util.Set;
+
+public record UserDTO (
+    Long id,
+    String username,
+    String email,
+    String phoneNumber,
+    String name,
+    String surname,
+    Set<RoleType> roles,
+    Long clinic,
+    String schedule,
+    String qualification,
+    String workingHours) {
+}
